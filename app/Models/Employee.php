6eaaa0e151d2cc12a936883f56dc79489591d139
IntrovertDev <tobiemman1@gmail.com>
2022-05-26 17:49:38 +0100

    Set up Laravel Models, Controllers and Migrations for API

---
+++ app/Models/Employee.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'employee_id',
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'role',
+        'username',
+        'password',
+        'permission'
+    ];
+}
